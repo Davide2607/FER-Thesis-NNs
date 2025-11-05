840b78cc63aaacbfc92be96c044d130562af24ef
Dragos Buhnila <s331394@studenti.polito.it>
2025-11-05 13:59:06 +0100

    misc: clean

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -4,6 +4,3 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == "__main__":
     print("hello_world")
-
-    from modules.hello_world import hello_world
-    print(hello_world())
